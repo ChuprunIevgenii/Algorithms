e714c8c8dfe97130b701f0b913e6d17275825719
Yevhenii Chuprun <chuprun.ievgenii@gmail.com>
2023-10-21 15:31:07 +0200

    IndexedBinaryHeap - fixed several issues

--- BinaryHeap/IndexedBinaryHeap.js
+++ BinaryHeap/IndexedBinaryHeap.js
@@ -5,9 +5,10 @@ class IndexedBinaryHeap {
         this.isMax = true; // max binary heap
     }
     enqueue(value) {
-        const length = this.values.push(value);
-        positions[value] = length - 1;
+        this.values.push(value);
+        this.positions[value] = this.values.length - 1;
         this.bubbleUp();
+        return this;
     }
     dequeue(value) {
 
@@ -21,7 +22,7 @@ class IndexedBinaryHeap {
         this.swap(currentIndex, lastIndex);
         delete this.positions[value];
         this.values.pop();
-        
+
         //tweak invariant
         if(this.values[currentIndex] <= this.values[parentIndex]) {
             this.bubbleUp(currentIndex);
@@ -32,7 +33,7 @@ class IndexedBinaryHeap {
         return true;
     }
     contains(value) {
-        return !!this.positions[value];
+        return this.positions[value] !== undefined;
     }
     heapify() {}
     sort() {}
@@ -49,7 +50,7 @@ class IndexedBinaryHeap {
     bubbleUp(index = this.values.length - 1) {
         let currentIndex = index;
 
-        while(currentIndex >= 0) {
+        while(currentIndex > 0) {
             const parentIndex = Math.ceil(currentIndex / 2) - 1;
 
             if(this.values[currentIndex] <= this.values[parentIndex]) return;
@@ -60,7 +61,7 @@ class IndexedBinaryHeap {
     }
     bubbleDown(index) {
         let parentIndex = index;
-    
+        
         while(parentIndex < this.values.length) {
             
             const leftChildIndex = parentIndex * 2 + 1;
@@ -74,15 +75,24 @@ class IndexedBinaryHeap {
             if(leftChildValue !== undefined && rightChildValue !== undefined) {
                 childLargestIndex = leftChildValue > rightChildValue ? leftChildIndex : rightChildIndex;
             } else if(leftChildValue !== undefined) {
-                childLargestIndex = leftChildValue;
+                childLargestIndex = leftChildIndex;
             }
 
-            if (largestChildIndex === null || this.values[parentIndex] > this.values[largestChildIndex]) {
+            if(childLargestIndex === null || this.values[parentIndex] > this.values[childLargestIndex]) {
                 return;
             }
-            
+
             this.swap(childLargestIndex, parentIndex);
+
             parentIndex = childLargestIndex;
         }
    }
 }
+
+const maxHeap = new IndexedBinaryHeap();
+
+maxHeap.enqueue(20).enqueue(16).enqueue(24).enqueue(25).enqueue(100);
+console.log(maxHeap.values);
+console.log(maxHeap.dequeue(100))
+console.log(maxHeap.values);
+console.log(maxHeap.positions);
